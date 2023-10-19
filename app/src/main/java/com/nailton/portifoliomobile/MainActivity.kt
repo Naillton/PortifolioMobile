494b9b4f4cb1e8fc852a3d4d2bf79198514deec3
Naillton <nailton_junior@protonmail.com>
2023-10-19 03:38:52 -0300

    finish Mobile Portifolio

--- app/src/main/java/com/nailton/portifoliomobile/MainActivity.kt
+++ app/src/main/java/com/nailton/portifoliomobile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nailton.portifoliomobile
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -8,8 +9,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,7 +25,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -35,17 +37,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -56,7 +58,6 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDirection
-import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -86,7 +87,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val colorList = listOf(Color(0xFF421fdf), Color(0xFF0c22de))
+    val stateCross by rememberSaveable { mutableStateOf(true) }
     val state = rememberScrollState()
+    val state1 = rememberScrollState()
+    val state2 = rememberScrollState()
     LaunchedEffect(Unit) { state.animateScrollTo(100)}
     val constraint = myConstraintSet()
     val context = LocalContext.current
@@ -116,7 +120,7 @@ fun MainScreen() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Content(constraint = constraint, modifier = Modifier)
+                Content(constraint = constraint, modifier = Modifier, stateCross, state1, state2)
             }
             ContactBottom(constraint, context)
         }
@@ -124,9 +128,17 @@ fun MainScreen() {
 }
 
 @Composable
-private fun Content(constraint: ConstraintSet, modifier: Modifier) {
+private fun Content(
+    constraint: ConstraintSet,
+    modifier: Modifier,
+    state: Boolean,
+    state1: ScrollState,
+    state2: ScrollState
+) {
     Header(constraint, modifier)
     ContentBody(constraint, modifier)
+    contentBodyProjectWeb(constraint, modifier, state, state2)
+    contentBodyProjectMobile(constraint, modifier, state, state1)
 }
 
 @Composable
@@ -156,41 +168,144 @@ private fun Header(constraint: ConstraintSet, modifier: Modifier) {
 @Composable
 private fun ContentBody(constraint: ConstraintSet, modifier: Modifier) {
     ConstraintLayout(
-        constraint,
-        modifier
-            .height(intrinsicSize = IntrinsicSize.Min)
-            .fillMaxWidth()
-            .layoutId("contentBody")
+        constraint
     ) {
-        TitleText(text = "Sobre Mim", modifier = Modifier.layoutId("contentBodyTitleText"))
-
-        Text(
-            buildAnnotatedString {
-                withStyle(
-                    style = ParagraphStyle(
-                        textAlign = TextAlign.Justify,
-                        textMotion = TextMotion.Animated,
-                        textDirection = TextDirection.Content
-                    )
-                ) {
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 20.sp,
-                            color = Color(0xFFa5bac2),
-                        )
-                    ) {
-                        append("Olá seja bem vindo!, me chamo Nailton tenho 24 anos e sou desenvolvedor,\n" +
-                                "gosto muito de jogos - principalmente rpg - e sou apaixonado por programação e hacking, " +
-                                "tenho muito interesse em desenvolver minhas habilidades ao máximo e no processo aprender coisas novas, " +
-                                "em meus projetos uso diferentes linguagens de programação, gosto muito de programar para mobile com kotlin\n" +
-                                "e react native, spring para backend e React para front-end Web.")
-                    }
-                }
-            },
+        Box(
             modifier
-                .padding(12.dp)
-                .layoutId("contentBodyText")
+                .height(intrinsicSize = IntrinsicSize.Min)
+                .fillMaxWidth()
+                .layoutId("contentBody"),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TitleText(text = "Sobre Mim", modifier = Modifier.layoutId("contentBodyTitleText"))
+
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = ParagraphStyle(
+                                textAlign = TextAlign.Justify,
+                                textMotion = TextMotion.Animated,
+                                textDirection = TextDirection.Content
+                            )
+                        ) {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontSize = 20.sp,
+                                    color = Color(0xFFa5bac2),
+                                )
+                            ) {
+                                append("Olá seja bem vindo!, me chamo Nailton tenho 24 anos e sou desenvolvedor,\n" +
+                                        "gosto muito de jogos - principalmente rpg - e sou apaixonado por programação e hacking, " +
+                                        "tenho muito interesse em desenvolver minhas habilidades ao máximo e no processo aprender coisas novas, " +
+                                        "em meus projetos uso diferentes linguagens de programação, gosto muito de programar para mobile com kotlin\n" +
+                                        "e react native, spring para backend e React para front-end Web.")
+                            }
+                        }
+                    },
+                    modifier
+                        .padding(12.dp)
+                        .layoutId("contentBodyText")
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun contentBodyProjectWeb(
+    constraint: ConstraintSet,
+    modifier: Modifier,
+    state: Boolean,
+    state2: ScrollState) {
+    val wApps: MutableList<Int> = arrayListOf(
+        R.drawable.apiquestion,
+        R.drawable.webgeneratepassword,
+        R.drawable.webloginexpress,
+        R.drawable.webplanetsearch,
+        R.drawable.webportifolio,
+        R.drawable.webrecipesapp
+    )
+    ConstraintLayout(
+        constraint
+    ) {
+
+        Box(
+            modifier
+                .fillMaxWidth()
+                .height(intrinsicSize = IntrinsicSize.Min)
+                .layoutId("contentBodyProjectWeb")
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TitleText(text = "Web Project", modifier = Modifier.layoutId("contentBodyPWTitle"))
+
+                Row(
+                    modifier.horizontalScroll(state2)
+                ) {
+                    ImageProject(state = state, images = wApps)
+                }
+            }
+
+        }
+    }
+}
+
+@SuppressLint("UnusedCrossfadeTargetStateParameter")
+@Composable
+private fun contentBodyProjectMobile(
+    constraint: ConstraintSet,
+    modifier: Modifier,
+    state: Boolean,
+    state1: ScrollState
+) {
+    val mApps: MutableList<Int> = arrayListOf(
+        R.drawable.mobileconsultas,
+        R.drawable.desktoploginscreen,
+        R.drawable.mobilenoteapp,
+        R.drawable.mobileppt,
+        R.drawable.mobileportifolio
+    )
+    ConstraintLayout(
+        constraint
+    ) {
+
+        Box(
+            modifier
+                .fillMaxWidth()
+                .height(intrinsicSize = IntrinsicSize.Min)
+                .layoutId("contentBodyProjectMobile")
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TitleText(
+                    text = "Mobile Project",
+                    modifier = Modifier.layoutId("contentBodyPMTitle")
+                )
+                Row(
+                    modifier.horizontalScroll(state1)
+                ) {
+                    ImageProject(state = state, images = mApps)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ImageProject(state: Boolean, images: MutableList<Int>) {
+    for (i in images) {
+        Image(
+            painter = painterResource(i),
+            contentDescription = "Mobile Apps",
+            Modifier
+                .size(400.dp)
+                .padding(20.dp)
         )
     }
 }
@@ -248,7 +363,8 @@ private fun ContactBottom(constraint: ConstraintSet, context: Context) {
             Modifier.layoutId("contentContact")
         ) {
             Row(
-                Modifier.fillMaxWidth()
+                Modifier
+                    .fillMaxWidth()
                     .height(80.dp)
                     .border(
                         border = BorderStroke(2.dp, Color.Black),
@@ -317,7 +433,7 @@ private fun ImageButton(
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color.Transparent,
                 contentColor = Color.Transparent
-                ),
+            ),
         ) {
             Column(
                 verticalArrangement = Arrangement.Center,
@@ -339,22 +455,6 @@ private fun ImageButton(
     }
 }
 
-@Composable
-private fun ScreenSizeHelper() {
-
-    val context = LocalContext.current
-
-    val displayMetrics = context.resources.displayMetrics
-
-    //Width And Height Of Screen
-    val width = displayMetrics.widthPixels
-    val height = displayMetrics.heightPixels
-
-    //Device Density
-    val density = displayMetrics.density
-
-}
-
 private fun myConstraintSet(): ConstraintSet {
     return ConstraintSet {
         val imagePerfil = createRefFor("imagePerfil")
@@ -366,6 +466,10 @@ private fun myConstraintSet(): ConstraintSet {
         val contentContact = createRefFor("contentContact")
         val contentButonContact = createRefFor("contentButonContact")
         val scrollId = createRefFor("scrollId")
+        val contentBodyProjectWeb = createRefFor("contentBodyProjectWeb")
+        val contentBodyPWTitle = createRefFor("contentBodyPWTitle")
+        val contentBodyProjectMobile = createRefFor("contentBodyProjectMobile")
+        val contentBodyPMTitle = createRefFor("contentBodyPMTitle")
 
         constrain(scrollId) {
             top.linkTo(parent.top)
@@ -393,7 +497,7 @@ private fun myConstraintSet(): ConstraintSet {
 
         constrain(contentBodyTitleText) {
             top.linkTo(parent.top, 20.dp)
-            centerHorizontallyTo(scrollId)
+            centerHorizontallyTo(contentBody)
         }
 
         constrain(contentBodyText) {
@@ -408,6 +512,25 @@ private fun myConstraintSet(): ConstraintSet {
         constrain(contentButonContact) {
             top.linkTo(parent.top)
         }
+
+        constrain(contentBodyProjectWeb) {
+            top.linkTo(contentBody.bottom)
+            centerHorizontallyTo(parent)
+        }
+
+        constrain(contentBodyPWTitle) {
+            top.linkTo(parent.top)
+        }
+
+        constrain(contentBodyProjectMobile) {
+            top.linkTo(contentBodyProjectWeb.bottom)
+            centerHorizontallyTo(parent)
+        }
+
+        constrain(contentBodyPMTitle) {
+            top.linkTo(parent.top)
+        }
+
     }
 }
 
